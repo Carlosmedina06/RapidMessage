8319f53ba27174b3870e33187b90e2f9cbbb0252
carlosmedina06 <slaawmedina14@gmail.com>
2023-05-14 23:22:17 -0300

    [Add] AuthContext

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { Inter } from 'next/font/google'
 
 import ToasterContext from './context/ToasterContext'
+import AuthContext from './context/AuthContext'
 
 import './globals.css'
 
@@ -15,8 +16,10 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ToasterContext />
-        {children}
+        <AuthContext>
+          <ToasterContext />
+          {children}
+        </AuthContext>
       </body>
     </html>
   )
