02f69ac66185cb3735fd775c2a122c6bdd9f7e60
carlosmedina06 <slaawmedina14@gmail.com>
2023-05-16 00:27:20 -0300

    [Add] User Layout

--- src/app/users/layout.tsx
+++ src/app/users/layout.tsx
@@ -1,10 +1,18 @@
 import SideBar from '../components/sidebar/Sidebar'
+import getUsers from '../actions/getUsers'
+
+import Userlist from './components/Userlist'
 
 export default async function UserLayout({ children }: { children: React.ReactNode }) {
+  const users = await getUsers()
+
   return (
     // @ts-expect-error Server Component
     <SideBar>
-      <div className="h-full">{children}</div>
+      <div className="h-full">
+        <Userlist items={users} />
+        {children}
+      </div>
     </SideBar>
   )
 }
