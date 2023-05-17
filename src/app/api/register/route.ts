ce671d27190526c4b3cc95ec5656cfd373adf0f3
carlosmedina06 <slaawmedina14@gmail.com>
2023-05-16 22:35:42 -0300

    [Update] format commit

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -4,28 +4,19 @@ import { NextResponse } from 'next/server'
 import prisma from '@/app/libs/prismadb'
 
 export async function POST(request: Request) {
-  try {
-    const { email, name, password } = await request.json()
+  const { email, name, password } = await request.json()
 
-    if (!email || !name || !password) {
-      return new NextResponse('Missing information', { status: 400 })
-    }
+  if (!email || !name || !password) return new NextResponse('Missing information', { status: 400 })
 
-    const hashedPassword = await bcrypt.hash(password, 12)
+  const hashedPassword = await bcrypt.hash(password, 12)
 
-    const user = await prisma.user.create({
-      data: {
-        email,
-        name,
-        hashedPassword,
-      },
-    })
+  const user = await prisma.user.create({
+    data: {
+      email,
+      name,
+      hashedPassword,
+    },
+  })
 
-    return NextResponse.json(user)
-  } catch (error: any) {
-    // eslint-disable-next-line no-console
-    console.error(error, 'REGISTER_ERROR') // <--- This will be logged to the terminal
-
-    return new NextResponse(error.message, { status: 500 })
-  }
+  return NextResponse.json(user)
 }
